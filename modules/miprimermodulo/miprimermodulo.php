f19eeb1025932d861f3808c93ae5d515b009e3c6
arullo <arullo@webimpacto.es>
2020-05-21 18:14:48 +0200

    WEB-2090 Usando WebService get

--- modules/miprimermodulo/miprimermodulo.php
+++ modules/miprimermodulo/miprimermodulo.php
@@ -193,7 +193,7 @@ class Miprimermodulo extends Module
 
         // try
         // {
-            $webService = new PrestaShopWebservice('http://localhost/mitiendanueva/', 'LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3X', false);
+            $webService = new PrestaShopWebservice('http://localhost/mitiendanueva/', 'K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM', false);
             
             // Here we set the option array for the Webservice : we want products resources
             $opt = [
@@ -203,32 +203,88 @@ class Miprimermodulo extends Module
             //dump($opt);
             // Call
             $xml = $webService->get($opt);
-            dump('xml');
-            dump($xml);
+            // dump('xml');
+            // dump($xml);
             // Here we get the elements from children of customers markup "products"
             $resources = $xml->products->children();
-            dump('resources');
-            dump($resources);
+            // dump('resources');
+            // dump($resources);
 
-            $apiKey = `LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3X`;
-            $authorizationKey = base64_encode($apiKey.':'); // LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3
-            dump($authorizationKey);
+            //$apiKey = `K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM`;
+            $authorizationKey = base64_encode('K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM:'); // K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM
+            // dump('keyyyy');
+            // dump($authorizationKey);
             foreach ($resources as $resource) {
-                dump('resource');
-                dump($resource);
-                $producttt= new Product($resource);
-                dump('producttttt');
-                dump($producttt);
+                //$webServiceId = new PrestaShopWebservice('http://localhost/mitiendanueva/products/'.$resourceId, 'LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3X', false);
+                // dump('resource');
+                // dump($resource);
                 $attributes = $resource->attributes();
-                dump('attributes');
-                dump($attributes);
-                $resourceId = $attributes['id'];
-                dump('resourceId');
-                dump($resourceId);
+                // dump('attributes');
+                // dump($attributes);
+                $resourceId = ((int)$attributes['id']);
+                // dump('resourceId');
+                // dump($resourceId);
+
+                //$webService = new PrestaShopWebservice('http://localhost/mitiendanueva/', 'K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM', false);
+                $webServiceId = new PrestaShopWebservice('http://localhost/mitiendanueva/', 'K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM', false);
+                //$webServiceId = new PrestaShopWebservice('http://localhost/mitiendanueva/products/', 'LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3X', false);
+
+                $optId = [
+                    'resource' => 'products',
+                    'id'  => $resourceId
+                ];
+                // dump('optId');
+                // dump($optId);
+
+                $xmlId = $webServiceId->get($optId);
+                // dump('xmlId');
+                // dump($xmlId);
+
+                $resourcesId = $xmlId->product->children();
+                // dump('resourcesId');
+                // dump($resourcesId);
+                // $nombre = $resourcesId->name->language[0];
+                // dump('nombre de producto');
+                // dump($nombre);
+                // $sgProduct = new Product(1);
+                $nlProduct = new Product(); // Remove ID later
+                    $nlProduct->id_category_default = $resourcesId->id_category_default;
+                    dump('categoria por defecto');
+                    dump($nlProduct->id_category_default);
+                    $nlProduct->id_category=$nlProduct->id_category_default;
+                    dump('categoria id');
+                    dump($nlProduct->id_category);
+                    $nlProduct->new;
+                    $nlProduct->type = $resourcesId->type;
+                    dump('type');
+                    dump($nlProduct->type);
+                    $nlProduct->reference = $resourcesId->reference;
+                    $nlProduct->weight =  $resourcesId->weight;
+                    // $nlProduct->price = ceil($input->getArgument('precio'));
+                    $nlProduct->price = $resourcesId->price;
+                    //$output->writeln('Precio: '.$nlProduct->price);
+                    $nlProduct->wholesale_price = $resourcesId->wholesale_price;
+                    $nlProduct->active = $resourcesId->active;
+                    $nlProduct->available_for_order = $resourcesId->available_for_order;
+                    $nlProduct->show_price = $resourcesId->show_price;
+                    $languages = Language::getLanguages();
+                    foreach($languages as $lang){
+                        
+                        //  $nlProduct->name[$lang['id_lang']] = $sgProduct->name->language;
+                        //  $nlProduct->description[$lang['id_lang']] = $sgProduct->description->language;
+                        $nlProduct->name[$lang['id_lang']] = $resourcesId->name->language[0];
+                        //$output->writeln('Hello Word! Nombre Language '.$lang['name'].' es: '.$nlProduct->name[$lang['id_lang']].' ');
+                        $nlProduct->description[$lang['id_lang']] = $resourcesId->description->language[0];
+                        //$output->writeln('Hello Word! Description Language '.$lang['name'].' es: '.$nlProduct->description[$lang['id_lang']].' ');
+                    }
+                    // echo($sgProduct->name->language);
+                    //$nlProduct->add();
+
                 
+                // }
                 //$url = 'http://LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3X@localhost/mitiendanueva/api/products/'.$resourceId;
                  //$url = 'http://'.$authorizationKey.'@localhost/mitiendanueva/api/products/'.$resourceId;
-                 dump($url);
+                 //dump($url);
                 // dump('url');
                 // dump($url);
                 // From there you could, for example, use th resource ID to call the webservice to get its details
--- modules/miprimermodulo/src/Command/CrearProductoCommand.php
+++ modules/miprimermodulo/src/Command/CrearProductoCommand.php
@@ -8,9 +8,10 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Webimpacto\MiPrimerModulo\Command\PSWebServiceLibrary;
 use Language;
 use Product;
-use WebserviceRequest;
+
 
 class CrearProductoCommand extends Command
 {
@@ -18,58 +19,161 @@ class CrearProductoCommand extends Command
     {
         // The name of the command (the part after "bin/console")
         $this->setName('miprimermodulo:create');
-        $this->addArgument('name', InputArgument::REQUIRED, '¿Escriba el nombre?');
-        $this->addArgument('description', InputArgument::REQUIRED, '¿Escriba la descripcion?');
-        $this->addArgument('precio', InputArgument::REQUIRED,'Escriba el precio');
+        // $this->addArgument('name', InputArgument::REQUIRED, '¿Escriba el nombre?');
+        // $this->addArgument('description', InputArgument::REQUIRED, '¿Escriba la descripcion?');
+        // $this->addArgument('precio', InputArgument::REQUIRED,'Escriba el precio');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         //GET productos de otro Presta
-        //$webService = new PrestaShopWebservice('http://localhost/mitiendanueva/', 'LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3X', false); 
         
-            $webService = new PrestaShopWebservice('http://localhost/mitiendanueva/', 'LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3X', false);
+        // try
+        // {
+            $webService = new PrestaShopWebservice('http://localhost/mitiendanueva/', 'K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM', false);
             
-            // Here we set the option array for the Webservice : we want customers resources
-            $opt['resource'] = 'products';
-            
+            // Here we set the option array for the Webservice : we want products resources
+            $opt = [
+                'resource' => 'products',
+                'filter[id]'  => '[20|21]'
+            ];
+            //dump($opt);
             // Call
             $xml = $webService->get($opt);
-        
-            // Here we get the elements from children of customers markup "customer"
-            $resources = $xml->customers->children();
+            // dump('xml');
+            // dump($xml);
+            // Here we get the elements from children of customers markup "products"
+            $resources = $xml->products->children();
+            // dump('resources');
+            // dump($resources);
+
+            //$apiKey = `K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM`;
+            $authorizationKey = base64_encode('K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM'); // K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM
+            // dump('keyyyy');
+            // dump($authorizationKey);
+            foreach ($resources as $resource) {
+                //$webServiceId = new PrestaShopWebservice('http://localhost/mitiendanueva/products/'.$resourceId, 'LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3X', false);
+                // dump('resource');
+                // dump($resource);
+                $attributes = $resource->attributes();
+                // dump('attributes');
+                // dump($attributes);
+                $resourceId = ((int)$attributes['id']);
+                // dump('resourceId');
+                // dump($resourceId);
+
+                //$webService = new PrestaShopWebservice('http://localhost/mitiendanueva/', 'K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM', false);
+                $webServiceId = new PrestaShopWebservice('http://localhost/mitiendanueva/', 'K84GDDGVJPRI7M6IKYPLKCJ1W6BQCTEM', false);
+                //$webServiceId = new PrestaShopWebservice('http://localhost/mitiendanueva/products/', 'LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3X', false);
+
+                $optId = [
+                    'resource' => 'products',
+                    'id'  => $resourceId
+                ];
+                // dump('optId');
+                // dump($optId);
+
+                $xmlId = $webServiceId->get($optId);
+                // dump('xmlId');
+                // dump($xmlId);
+
+                $resourcesId = $xmlId->product->children();
+                // dump('resourcesId');
+                // dump($resourcesId);
+                // $nombre = $resourcesId->name->language[0];
+                // dump('nombre de producto');
+                // dump($nombre);
+                // $sgProduct = new Product(1);
+                $nlProduct = new Product(); // Remove ID later
+                    $nlProduct->id_category_default = $resourcesId->id_category_default;
+                    dump('categoria por defecto');
+                    dump($nlProduct->id_category_default);
+                    $nlProduct->id_category=$nlProduct->id_category_default;
+                    dump('categoria id');
+                    dump($nlProduct->id_category);
+                    $nlProduct->new;
+                    $nlProduct->type = $resourcesId->type;
+                    dump('type');
+                    dump($nlProduct->type);
+                    $nlProduct->reference = $resourcesId->reference;
+                    $nlProduct->weight =  $resourcesId->weight;
+                    // $nlProduct->price = ceil($input->getArgument('precio'));
+                    $nlProduct->price = $resourcesId->price;
+                    //$output->writeln('Precio: '.$nlProduct->price);
+                    $nlProduct->wholesale_price = $resourcesId->wholesale_price;
+                    $nlProduct->active = $resourcesId->active;
+                    $nlProduct->available_for_order = $resourcesId->available_for_order;
+                    $nlProduct->show_price = $resourcesId->show_price;
+                    $languages = Language::getLanguages();
+                    foreach($languages as $lang){
+                        
+                        //  $nlProduct->name[$lang['id_lang']] = $sgProduct->name->language;
+                        //  $nlProduct->description[$lang['id_lang']] = $sgProduct->description->language;
+                        $nlProduct->name[$lang['id_lang']] = $resourcesId->name->language[0];
+                        //$output->writeln('Hello Word! Nombre Language '.$lang['name'].' es: '.$nlProduct->name[$lang['id_lang']].' ');
+                        $nlProduct->description[$lang['id_lang']] = $resourcesId->description->language[0];
+                        //$output->writeln('Hello Word! Description Language '.$lang['name'].' es: '.$nlProduct->description[$lang['id_lang']].' ');
+                    }
+                    // echo($sgProduct->name->language);
+                    $nlProduct->add();
+
+                
+                // }
+                //$url = 'http://LZG9E7EVJ6FS1E7TCVAVCESCMXZMHC3X@localhost/mitiendanueva/api/products/'.$resourceId;
+                 //$url = 'http://'.$authorizationKey.'@localhost/mitiendanueva/api/products/'.$resourceId;
+                 //dump($url);
+                // dump('url');
+                // dump($url);
+                // From there you could, for example, use th resource ID to call the webservice to get its details
+            }
+            //dump($resources);
+        //}
+        // catch (PrestaShopWebserviceException $e)
+        // {
+        //     // // Here we are dealing with errors
+        //     // $trace = $e->getTrace();
+        //     // if ($trace[0]['args'][0] == 404) echo 'Bad ID';
+        //     // else if ($trace[0]['args'][0] == 401) echo 'Bad auth key';
+        //     // else echo 'Other error';
+        // }
 
 
 
         //FIN GET productos de otro Presta
-        $sgProduct = new Product(1);
-        $nlProduct = new Product(); // Remove ID later
-            $nlProduct->id_category_default = 2;
-            $nlProduct->id_category[]=$nlProduct->id_category_default;
-            $nlProduct->new;
-            $nlProduct->type = "simple";
-            $nlProduct->reference = $sgProduct->reference;
-            $nlProduct->weight =  $sgProduct->weight;
-            // $nlProduct->price = ceil($input->getArgument('precio'));
-            $nlProduct->price = $input->getArgument('precio');
-            $output->writeln('Precio: '.$nlProduct->price);
-            $nlProduct->wholesale_price = $sgProduct->wholesale_price;
-            $nlProduct->active = 0;
-            $nlProduct->available_for_order = 1;
-            $nlProduct->show_price = 1;
-            $languages = Language::getLanguages();
-            foreach($languages as $lang){
-                //  $nlProduct->name[$lang['id_lang']] = $sgProduct->name->language;
-                //  $nlProduct->description[$lang['id_lang']] = $sgProduct->description->language;
-                $nlProduct->name[$lang['id_lang']] = $input->getArgument('name');
-                $output->writeln('Hello Word! Nombre Language '.$lang['name'].' es: '.$nlProduct->name[$lang['id_lang']].' ');
-                $nlProduct->description[$lang['id_lang']] = $input->getArgument('description');
-                $output->writeln('Hello Word! Description Language '.$lang['name'].' es: '.$nlProduct->description[$lang['id_lang']].' ');
-            }
-            // echo($sgProduct->name->language);
-            $nlProduct->add();
-            //$nlProduct->update();
-        
+
+        //Crear Productos
+
+        // $sgProduct = new Product(1);
+        // $nlProduct = new Product(); // Remove ID later
+        //     $nlProduct->id_category_default = 2;
+        //     $nlProduct->id_category[]=$nlProduct->id_category_default;
+        //     $nlProduct->new;
+        //     $nlProduct->type = "simple";
+        //     $nlProduct->reference = $sgProduct->reference;
+        //     $nlProduct->weight =  $sgProduct->weight;
+        //     // $nlProduct->price = ceil($input->getArgument('precio'));
+        //     $nlProduct->price = $input->getArgument('precio');
+        //     $output->writeln('Precio: '.$nlProduct->price);
+        //     $nlProduct->wholesale_price = $sgProduct->wholesale_price;
+        //     $nlProduct->active = 0;
+        //     $nlProduct->available_for_order = 1;
+        //     $nlProduct->show_price = 1;
+        //     $languages = Language::getLanguages();
+        //     foreach($languages as $lang){
+        //         //  $nlProduct->name[$lang['id_lang']] = $sgProduct->name->language;
+        //         //  $nlProduct->description[$lang['id_lang']] = $sgProduct->description->language;
+        //         $nlProduct->name[$lang['id_lang']] = $input->getArgument('name');
+        //         $output->writeln('Hello Word! Nombre Language '.$lang['name'].' es: '.$nlProduct->name[$lang['id_lang']].' ');
+        //         $nlProduct->description[$lang['id_lang']] = $input->getArgument('description');
+        //         $output->writeln('Hello Word! Description Language '.$lang['name'].' es: '.$nlProduct->description[$lang['id_lang']].' ');
+        //     }
+        //     // echo($sgProduct->name->language);
+        //     $nlProduct->add();
+        //     //$nlProduct->update();
+
+         // FinCrear Productos
+
+
         $output->writeln('Hello Word! id_Product es: '.$nlProduct->id.' ');
         //  $output->writeln('Hello Word! id_Product es: '.$nlProduct->name.' ');
         // $output->writeln('Hello Word! id_Product es: '.$nlProduct->name.' ');
