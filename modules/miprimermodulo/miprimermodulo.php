66e58b4fb206fd0b2daf44c6dee6dba0e0006ef5
arullo <arullo@webimpacto.es>
2020-05-25 17:48:41 +0200

    WEB-2090 Agregando con comando productos de otra tienda con API

--- modules/miprimermodulo/miprimermodulo.php
+++ modules/miprimermodulo/miprimermodulo.php
@@ -192,14 +192,11 @@ class Miprimermodulo extends Module
         // $this->context->smarty->assign(array('categoria_name' => $categoria_name,));
         //$imagenes2 = new Product($params['product']);
 
-
-
-
         
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-          CURLOPT_URL => "localhost/mitiendanueva/api/products/20?output_format=JSON",
+          CURLOPT_URL => "localhost/mitiendanueva/api/products?output_format=JSON",
           CURLOPT_RETURNTRANSFER => true,
           CURLOPT_ENCODING => "",
           CURLOPT_MAXREDIRS => 10,
@@ -216,44 +213,162 @@ class Miprimermodulo extends Module
         $response = curl_exec($curl);
         
         curl_close($curl);
-            dump($response);
+            //dump($response);
             //$productJSON = new Product();
 
             $array = json_decode($response,true);
-            dump($array);
-             //$productJSON= $array['product'];
+            dump($array['products']);
+            
+            foreach($array['products'] as $productoArrayFuera){
+                dump('producto array fueraaaaa');
+                dump($productoArrayFuera);
+                $curlId = curl_init();
+                curl_setopt_array($curlId, array(
+                    CURLOPT_URL => "localhost/mitiendanueva/api/products/".$productoArrayFuera["id"]."?output_format=JSON",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "GET",
+                    CURLOPT_HTTPHEADER => array(
+                      "Authorization: Basic Szg0R0RER1ZKUFJJN002SUtZUExLQ0oxVzZCUUNURU06Oc ",
+                      "Cookie: PrestaShop-cbac049c40e842298a95cd1e70b00bde=def50200361ad098563fc2ae544f931676d4d44b5da873585c5ca6b0349e123fdf3330aede70df11b574eae14ae3c38561487679817d13b7095332c7f43c81a49c7f11f0cdd3987d220c3e3ee00c71ad4551c7cbc36e5fa962865788f32303de42f1323f5f103bc86407a0a0cc87b3e43a224e7333192e7028eb678a89f2183cf85b04832ae0b59162a344103517cae9dd7afb4b7135a942cf741fb3214f55429596"
+                    ),
+                  ));
+                  $responseId = curl_exec($curlId);
+                  
+                  dump($responseId);
+                    curl_close($curlId);
+                    $arrayId = json_decode($responseId,true);
+                    dump($arrayId);
+
+
+                $productImport= $arrayId['product'];
+                //$productImport= $arrayId['product'];
+                dump('ProductoImportadooooooooooooooooooo');
+                dump($productImport);
+
+                $productos = new Product();
+                $arrayProductos =$productos->getProducts(1,0,21,'id_product','asc',false,false,null);
+                dump('Array Product');
+                dump($arrayProductos);
+                
+                $veces_igual= 0;
+                        foreach($arrayProductos as $productoArray){
+                            dump('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!dentro de foreach');
+                            // dump('Producto del array');
+                            // dump($productoArray);
+
+                            
+                            
+                            // $veces =0;
+                            // while ($veces <=21 || $veces_igual=0) {
+                            //     dump('dentro de while!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+                                dump('buscando');
+                                dump($productImport['reference']);
+                                dump('para ver si coincide con mis productos, por ejemplo este:');
+                                dump($productoArray['reference']);
+                                dump("igual");
+                                dump($veces_igual);
+                                // dump("veces");
+                                // dump($veces);
+                                if($productImport['reference'] == $productoArray['reference']){
+                                    $veces_igual++; 
+                                }
+                                //$veces++;
+                            //}
+                        }
+                    
+                    
+                if($veces_igual==0){
+                    dump('creamos');
+
+                     //$pro= $array['product'];
+
+                     $productJSON = new Product(); // Remove ID later
+                         $productJSON->id_category_default = $productImport['id_category_default'];
+                         dump($productJSON->id_category_default);
+                         $productJSON->id_category=$productImport['id_category_default'];
+                         $productJSON->new;
+                         $productJSON->type = $productImport['type'];
+                         $productJSON->reference = $productImport['reference'];
+                         $productJSON->weight =  $productImport['weight'];
+                         // $nlProduct->price = ceil($input->getArgument('precio'));
+                         $productJSON->price = $productImport['price'];
+                         //$output->writeln('Precio: '.$nlProduct->price);
+                         $productJSON->wholesale_price = $productImport['wholesale_price'];
+                         $productJSON->active = $productImport['active'];
+                         $productJSON->available_for_order = $productImport['available_for_order'];
+                         $productJSON->show_price = $productImport['show_price'];
+                         $languages = Language::getLanguages();
+                         foreach($languages as $lang){
+                             //  $nlProduct->name[$lang['id_lang']] = $sgProduct->name->language;
+                             //  $nlProduct->description[$lang['id_lang']] = $sgProduct->description->language;
+                             $productJSON->name[$lang['id_lang']] = $productImport['name'][0]['value'];
+                             //$output->writeln('Hello Word! Nombre Language '.$lang['name'].' es: '.$nlProduct->name[$lang['id_lang']].' ');
+                             $productJSON->description[$lang['id_lang']] = $productImport['description'][0]['value'];
+                             //$output->writeln('Hello Word! Description Language '.$lang['name'].' es: '.$nlProduct->description[$lang['id_lang']].' ');
+                         }
+
+
+
+                //$productJSON->add();
+
+                } else{
+                    dump('Esta repetido');
+                }                   
+                
+                
+            }
+
+
+//------------------------------------------------------------Productos mi tienda----------------------------------
+            
+             $productos = new Product();
+             $arrayProductos =$productos->getProducts(1,0,21,'id_product','asc',false,false,null);
+             dump('Array Product');
+             dump($arrayProductos);
+             foreach($arrayProductos as $productoArray){
+                dump('Producto del array');
+                dump($productoArray);
+                // if()
+            }
+//------------------------------------------------------------Productos mi tienda----------------------------------
+
 
          //dump($productJSON['name']);      // Dump all data of the Object
-         $pro= $array['product'];
+        //  $pro= $array['product'];
 
-         $productJSON = new Product(); // Remove ID later
-             $productJSON->id_category_default = $pro['id_category_default'];
-             dump($productJSON->id_category_default);
-             $productJSON->id_category=$pro['id_category_default'];
-             $productJSON->new;
-             $productJSON->type = $pro['type'];
-             $productJSON->reference = $pro['reference'];
-             $productJSON->weight =  $pro['weight'];
-             // $nlProduct->price = ceil($input->getArgument('precio'));
-             $productJSON->price = $pro['price'];
-             //$output->writeln('Precio: '.$nlProduct->price);
-             $productJSON->wholesale_price = $pro['wholesale_price'];
-             $productJSON->active = $pro['active'];
-             $productJSON->available_for_order = $pro['available_for_order'];
-             $productJSON->show_price = $pro['show_price'];
-             $languages = Language::getLanguages();
-             foreach($languages as $lang){
-                 //  $nlProduct->name[$lang['id_lang']] = $sgProduct->name->language;
-                 //  $nlProduct->description[$lang['id_lang']] = $sgProduct->description->language;
-                 $productJSON->name[$lang['id_lang']] = $pro['name'][0]['value'];
-                 //$output->writeln('Hello Word! Nombre Language '.$lang['name'].' es: '.$nlProduct->name[$lang['id_lang']].' ');
-                 $productJSON->description[$lang['id_lang']] = $pro['description'][0]['value'];
-                 //$output->writeln('Hello Word! Description Language '.$lang['name'].' es: '.$nlProduct->description[$lang['id_lang']].' ');
-             }
+        //  $productJSON = new Product(); // Remove ID later
+        //      $productJSON->id_category_default = $pro['id_category_default'];
+        //      dump($productJSON->id_category_default);
+        //      $productJSON->id_category=$pro['id_category_default'];
+        //      $productJSON->new;
+        //      $productJSON->type = $pro['type'];
+        //      $productJSON->reference = $pro['reference'];
+        //      $productJSON->weight =  $pro['weight'];
+        //      // $nlProduct->price = ceil($input->getArgument('precio'));
+        //      $productJSON->price = $pro['price'];
+        //      //$output->writeln('Precio: '.$nlProduct->price);
+        //      $productJSON->wholesale_price = $pro['wholesale_price'];
+        //      $productJSON->active = $pro['active'];
+        //      $productJSON->available_for_order = $pro['available_for_order'];
+        //      $productJSON->show_price = $pro['show_price'];
+        //      $languages = Language::getLanguages();
+        //      foreach($languages as $lang){
+        //          //  $nlProduct->name[$lang['id_lang']] = $sgProduct->name->language;
+        //          //  $nlProduct->description[$lang['id_lang']] = $sgProduct->description->language;
+        //          $productJSON->name[$lang['id_lang']] = $pro['name'][0]['value'];
+        //          //$output->writeln('Hello Word! Nombre Language '.$lang['name'].' es: '.$nlProduct->name[$lang['id_lang']].' ');
+        //          $productJSON->description[$lang['id_lang']] = $pro['description'][0]['value'];
+        //          //$output->writeln('Hello Word! Description Language '.$lang['name'].' es: '.$nlProduct->description[$lang['id_lang']].' ');
+        //      }
 
 
 
-     $productJSON->add();
+     //$productJSON->add();
 
 
 
